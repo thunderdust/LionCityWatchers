1c89a613a762c474b067a0d398e71fb6adaecf45
thunderdust <liuweiran.nus@gmail.com>
2015-10-28 02:03:49 +0800

    Add widgets initialization in report activity

--- app/src/main/java/com/example/thunderdust/lioncitywatchers/ReportActivity.java
+++ app/src/main/java/com/example/thunderdust/lioncitywatchers/ReportActivity.java
@@ -2,12 +2,86 @@ package com.example.thunderdust.lioncitywatchers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Button;
 
 public class ReportActivity extends AppCompatActivity {
+
+    private EditText mIncidentDescriptionET;
+    private ImageView mIncidentImageView;
+    private Button mCameraButton;
+    private Button mGalleryButton;
+    private Button mShareButton;
+    private Button mDiscardButton;
+    private Button mPostButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+        initializeWidgets();
+    }
+
+    private void initializeWidgets(){
+        mIncidentDescriptionET = (EditText) findViewById(R.id.report_description);
+        mIncidentImageView = (ImageView) findViewById(R.id.report_image_view);
+        mCameraButton = (Button) findViewById(R.id.btn_camera);
+        mGalleryButton = (Button) findViewById(R.id.btn_gallery);
+        mShareButton = (Button) findViewById(R.id.btn_report_share);
+        mDiscardButton = (Button) findViewById(R.id.btn_report_discard);
+        mPostButton = (Button) findViewById(R.id.btn_report_submit);
+
+        if (mCameraButton!=null){
+            mCameraButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            });
+        }
+
+        if(mGalleryButton!=null){
+            mGalleryButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                }
+            });
+        }
+
+        if(mShareButton!=null){
+            mShareButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                }
+            });
+        }
+
+        if(mDiscardButton!=null){
+            mDiscardButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    ReportActivity.this.finish();
+                    // alert report post discarded
+                }
+            });
+        }
+
+        if(mPostButton!=null){
+            mPostButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                }
+            });
+        }
+    }
+
+    private boolean shareToInstagram(){
+        return false;
+
+    }
+
+    private boolean postNewReport(){
+        return false;
     }
 }
